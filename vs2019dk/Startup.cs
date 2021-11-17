76b8326e0285a958761d5da9bb8f7895c960b4d1
Madhan Gopal <madhangopal9704@gmail.com>
2021-11-17 16:10:23 +0530

    captain

--- vs2019dk/Startup.cs
+++ vs2019dk/Startup.cs
@@ -33,6 +33,7 @@ namespace vs2019dk
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("do work");
                 });
             });
         }
